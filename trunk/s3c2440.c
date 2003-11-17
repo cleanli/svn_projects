0471ea1e6c761a10b1bcc1a4e8a3b97a5872f7b1
ert <ert@526fc652-ec01-754f-a60d-a281a7b4fbdd>
2003-11-17 18:26:58 +0000

    nand read is ready, and will realize the boot from jump to ram tomorrow
    git-svn-id: file:///tmp/clean_boot@3 526fc652-ec01-754f-a60d-a281a7b4fbdd

--- trunk/s3c2440.c
+++ trunk/s3c2440.c
@@ -1,6 +1,41 @@
 /*s3c2440.c:define the related func code for s3c2440*/
 
 #include "s3c2440.h"
+
+/* low level nand read function */
+int nand_read_ll(unsigned char *buf, unsigned long start_addr, int size)
+{
+        int i, j;
+
+        if ((start_addr & NAND_BLOCK_MASK) || (size & NAND_BLOCK_MASK)) {
+                return -1;      /* invalid alignment */
+        }
+
+        NAND_CHIP_ENABLE;
+
+        for(i=start_addr; i < (start_addr + size);) {
+                /* READ0 */
+                NAND_CLEAR_RB;
+                NFCMD = 0;
+
+                /* Write Address */
+                NFADDR = i & 0xff;
+                NFADDR = (i >> 9) & 0xff;
+                NFADDR = (i >> 17) & 0xff;
+                NFADDR = (i >> 25) & 0xff;
+
+                NAND_DETECT_RB;
+
+                for(j=0; j < NAND_SECTOR_SIZE; j++, i++) {
+                        *buf = (NFDATA & 0xff);
+                        buf++;
+                }
+		if(!((i>>9) & 0x3f))
+			s3c2440_serial_send_byte('>');
+        }
+        NAND_CHIP_DISABLE;
+        return 0;
+}
 /*
 void s3c2440_serial_init(){
 	GPHCON = 0xaaa0;
--- trunk/version.h
+++ trunk/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
-#define CLEAN_BOOT_VERSION "0.3(xmodem&dram&400)"
+#define CLEAN_BOOT_VERSION "0.4(xmodem&clock 405MHz&dram ok&nand copy)"
 
 #endif
