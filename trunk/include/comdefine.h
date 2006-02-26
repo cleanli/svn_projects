bf1621a80fcdd05cddc2da5a0dd89fc8796c44b7
Chenji <Chenji@5ed68377-c699-c041-91ca-6c13319bb34a>
2006-02-26 01:04:12 +0000

    change in comdefine.h
    git-svn-id: file:///tmp/codecollect/MyC51Lib@13 5ed68377-c699-c041-91ca-6c13319bb34a

--- trunk/include/comdefine.h
+++ trunk/include/comdefine.h
@@ -1,10 +1,11 @@
-#ifndef _DEFINE_H
-#define _DEFINE_H
+#ifndef _COM_DEFINE_H
+#define _COM_DEFINE_H
 
 #include <AT89X51.H>
 
 #define uch unsigned char
 #define uint unsigned int
+#define BOOL bit
 
 
 #endif
